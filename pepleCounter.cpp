2af0ca8e4c27249745e0b1797569eb5d80f857fd
nurulkairiza <nurulkhairiza@gmail.com>
2022-11-11 13:17:47 +0700

    edit peoplecounter

--- pepleCounter.cpp
+++ pepleCounter.cpp
@@ -4,43 +4,43 @@ int ppl = 0;
 bool pi = 0;
 bool po = 0;
 
-long int pi_read_at;
-long int po_read_at;
-
 void setupCounter(int in, int out) {
   pinMode(in, INPUT);
   pinMode(out, INPUT);
 }
 
 
-int loopCounter(int in, int out) {    
-  bool pi = digitalRead(in);
-  bool po = digitalRead(out);
-    if (pi == 0 && po == 1){
-      Serial.println("Reading people in");
-      while (true) {
-          po = digitalRead(out);
-          if (po == 0){
-            ppl++;
-            Serial.print("one people in. remaining ppl: ");
-            Serial.println(ppl);
-            break;
-          }
+int loopCounter(int in, int out) {
+  pi = digitalRead(in);
+  po = digitalRead(out);
+  if (pi == 0) {
+    // Serial.println("Reading people in");
+    while (true) {
+      po = digitalRead(out);
+      if (po == 0) {
+        ppl++;
+        // Serial.print("People in room: ");
+        // Serial.println(ppl);
+        delay(100);
+        break;
       }
     }
-    else if (pi == 1 && po == 0){
-      Serial.println("Reading people out");
-      while (true) {
-        pi = digitalRead(in);
-        if (pi == 0){
-          ppl--;
-          Serial.print("one people out. remaining ppl: ");
-          Serial.println(ppl);
-          break;
+  }
+  else if (po == 0) {
+    // Serial.println("Reading people out");
+    while (true) {
+      pi = digitalRead(in);
+      if (pi == 0) {
+        if (ppl > 0) {
+           ppl--; 
         }
+        // Serial.print("People in room: ");
+        // Serial.println(ppl);
+        delay(100);
+        break;
       }
     }
-
-    return ppl;
+  }
+  return ppl;
   
 }
